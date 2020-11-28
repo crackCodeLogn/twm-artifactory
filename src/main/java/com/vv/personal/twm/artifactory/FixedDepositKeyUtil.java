895089dac75ec193ed7c8a5053a713b212b8b891
Vivek <vivektheintel@gmail.com>
2020-11-28 18:34:53 +0530

    Generalizing fd key computation for proto and builder

--- src/main/java/com/vv/personal/twm/artifactory/FixedDepositKeyUtil.java
+++ src/main/java/com/vv/personal/twm/artifactory/FixedDepositKeyUtil.java
@@ -10,11 +10,15 @@ public class FixedDepositKeyUtil {
 
     public static final String FD_KEY = "%s-%s-%.2f-%s-%d";
 
+    private static String generateFdKey(String user, String ifsc, double depositAmount, String startDate, long insertionTime) {
+        return String.format(FD_KEY, user, ifsc, depositAmount, startDate, insertionTime);
+    }
+
     public static String generateFdKey(FixedDepositProto.FixedDeposit fixedDeposit) {
-        return String.format(FD_KEY, fixedDeposit.getUser(), fixedDeposit.getBankIFSC(), fixedDeposit.getDepositAmount(), fixedDeposit.getStartDate(), fixedDeposit.getInsertionTime());
+        return generateFdKey(fixedDeposit.getUser(), fixedDeposit.getBankIFSC(), fixedDeposit.getDepositAmount(), fixedDeposit.getStartDate(), fixedDeposit.getInsertionTime());
     }
 
     public static String generateFdKey(FixedDepositProto.FixedDeposit.Builder fixedDepositBuilder) {
-        return String.format(FD_KEY, fixedDepositBuilder.getUser(), fixedDepositBuilder.getBankIFSC(), fixedDepositBuilder.getDepositAmount(), fixedDepositBuilder.getStartDate(), fixedDepositBuilder.getInsertionTime());
+        return generateFdKey(fixedDepositBuilder.getUser(), fixedDepositBuilder.getBankIFSC(), fixedDepositBuilder.getDepositAmount(), fixedDepositBuilder.getStartDate(), fixedDepositBuilder.getInsertionTime());
     }
 }
