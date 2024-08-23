2064f152d28ff7e994762852744167d5530e4ccd
Vivek <vivektheintel@gmail.com>
2024-08-23 19:27:22 -0400

    Updating MarketData and Fixed Deposit to allow for account type

--- protobuf/generateFixedDepositProto.sh
+++ protobuf/generateFixedDepositProto.sh
@@ -1,3 +1,4 @@
 #To be run from this folder itself!
+rm ../src/main/java/com/vv/personal/twm/artifactory/generated/deposit/FixedDepositProto.java
 #Install protoc via 'sudo apt install protobuf-compiler'
 protoc --java_out='../src/main/java' FixedDeposit.proto
