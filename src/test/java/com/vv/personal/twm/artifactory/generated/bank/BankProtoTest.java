7ee3fef98ea5e12516ed1792809dfe8ad09702cc
Vivek <vivektheintel@gmail.com>
2024-09-10 21:25:07 -0400

    updating to junit 5

--- src/test/java/com/vv/personal/twm/artifactory/generated/bank/BankProtoTest.java
+++ src/test/java/com/vv/personal/twm/artifactory/generated/bank/BankProtoTest.java
@@ -1,32 +1,30 @@
 package com.vv.personal.twm.artifactory.generated.bank;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import static com.vv.personal.twm.artifactory.generated.bank.BankProto.BankType.PRIVATE;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Vivek
  * @since 27/11/20
  */
-@RunWith(JUnit4.class)
 public class BankProtoTest {
 
-    @Test
-    public void testBankObjectCreation() {
+  @Test
+  public void bankObjectCreation() {
 
-        BankProto.Bank bank = BankProto.Bank.newBuilder()
-                .setName("SBI")
-                .setBankType(PRIVATE)
-                .setContactNumber("123456789")
-                .setIFSC("SBI00102131")
-                .build();
+    BankProto.Bank bank =
+        BankProto.Bank.newBuilder()
+            .setName("SBI")
+            .setBankType(PRIVATE)
+            .setContactNumber("123456789")
+            .setIFSC("SBI00102131")
+            .build();
 
-        assertEquals("SBI", bank.getName());
-        assertEquals("123456789", bank.getContactNumber());
-        assertEquals("SBI00102131", bank.getIFSC());
-        assertEquals(PRIVATE, bank.getBankType());
-    }
+    assertEquals("SBI", bank.getName());
+    assertEquals("123456789", bank.getContactNumber());
+    assertEquals("SBI00102131", bank.getIFSC());
+    assertEquals(PRIVATE, bank.getBankType());
+  }
 }
--- src/test/java/com/vv/personal/twm/artifactory/generated/deposit/FixedDepositProtoTest.java
+++ src/test/java/com/vv/personal/twm/artifactory/generated/deposit/FixedDepositProtoTest.java
@@ -1,43 +1,41 @@
 package com.vv.personal.twm.artifactory.generated.deposit;
 
-import com.vv.personal.twm.artifactory.util.FixedDepositKeyUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import static com.vv.personal.twm.artifactory.generated.deposit.FixedDepositProto.InterestType.ON_MATURITY;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.vv.personal.twm.artifactory.util.FixedDepositKeyUtil;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Vivek
  * @since 27/11/20
  */
-@RunWith(JUnit4.class)
 public class FixedDepositProtoTest {
 
-    @Test
-    public void testFixedDepositCreation() {
-        FixedDepositProto.FixedDeposit fixedDeposit = FixedDepositProto.FixedDeposit.newBuilder()
-                .setUser("VIVEK")
-                .setBankIFSC("XYZ1223343")
-                .setDepositAmount(555555.23)
-                .setCustomerId("12324242")
-                .setFdNumber("3420940290492")
-                .setRateOfInterest(9.3)
-                .setStartDate("20201127")
-                .setEndDate("20251227")
-                .setMonths(61)
-                .setInterestType(ON_MATURITY)
-                .setNominee("ITACHI")
-                .build();
+  @Test
+  public void fixedDepositCreation() {
+    FixedDepositProto.FixedDeposit fixedDeposit =
+        FixedDepositProto.FixedDeposit.newBuilder()
+            .setUser("VIVEK")
+            .setBankIFSC("XYZ1223343")
+            .setDepositAmount(555555.23)
+            .setCustomerId("12324242")
+            .setFdNumber("3420940290492")
+            .setRateOfInterest(9.3)
+            .setStartDate("20201127")
+            .setEndDate("20251227")
+            .setMonths(61)
+            .setInterestType(ON_MATURITY)
+            .setNominee("ITACHI")
+            .build();
 
-        assertEquals("VIVEK", fixedDeposit.getUser());
-        assertEquals(ON_MATURITY, fixedDeposit.getInterestType());
-        assertEquals(555555.23, fixedDeposit.getDepositAmount(), 10E-6);
+    assertEquals("VIVEK", fixedDeposit.getUser());
+    assertEquals(ON_MATURITY, fixedDeposit.getInterestType());
+    assertEquals(555555.23, fixedDeposit.getDepositAmount(), 10E-6);
 
-        String fdKey = FixedDepositKeyUtil.generateFdKey(fixedDeposit);
-        System.out.println(fdKey);
-        assertTrue(fdKey.matches("VIVEK-XYZ1223343-555555.23-20201127-[0-9]+"));
-    }
+    String fdKey = FixedDepositKeyUtil.generateFdKey(fixedDeposit);
+    System.out.println(fdKey);
+    assertTrue(fdKey.matches("VIVEK-XYZ1223343-555555.23-20201127-[0-9]+"));
+  }
 }
