fee4b829ad109479ae040c9a39eb03f7dc5d96b2
Vivek <vivektheintel@gmail.com>
2024-12-23 22:22:02 -0500

    Introducing BankAccount message and adding country code to Bank message

--- protobuf/generateBankProto.sh
+++ protobuf/generateBankProto.sh
@@ -1,3 +1,4 @@
 #To be run from this folder itself!
+rm ../src/main/java/com/vv/personal/twm/artifactory/generated/bank/BankProto.java
 #Install protoc via 'sudo apt install protobuf-compiler'
 protoc --java_out='../src/main/java' Bank.proto
