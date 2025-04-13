6b70c4273b9c2940600552e3b257c6ec8d4887c9
Vivek <vivektheintel@gmail.com>
2025-04-13 00:10:26 -0400

    Rolling update to TwmUtil

--- src/main/java/com/vv/personal/twm/artifactory/util/TwmUtil.java
+++ src/main/java/com/vv/personal/twm/artifactory/util/TwmUtil.java
@@ -1,8 +1,12 @@
 package com.vv.personal.twm.artifactory.util;
 
 import com.google.common.hash.Hashing;
+import com.google.protobuf.Timestamp;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.UUID;
 
 /**
@@ -25,5 +29,29 @@ public class TwmUtil {
 
   public static String generateSha256Hash(String data) {
     return Hashing.sha256().hashString(data, StandardCharsets.UTF_8).toString();
+  }
+
+  public static boolean writeToFile(
+      String data, String destinationFolder, String destinationFileName) throws IOException {
+    String destinationFile = String.format("%s/%s", destinationFolder, destinationFileName);
+    try (FileWriter fileWriter = new FileWriter(destinationFile)) {
+      fileWriter.write(data);
+      return true;
+    } catch (IOException e) {
+      throw new IOException(
+          String.format(
+              "Failed to write content to file: %s. %s", destinationFile, e.getMessage()));
+    }
+  }
+
+  public static Instant fromTimestamp(Timestamp timestamp) {
+    return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+  }
+
+  public static Timestamp fromInstant(Instant instant) {
+    return Timestamp.newBuilder()
+        .setSeconds(instant.getEpochSecond())
+        .setNanos(instant.getNano())
+        .build();
   }
 }
